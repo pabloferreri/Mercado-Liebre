051a4ecb5e91325e9de1f3efa2a2c6e16832c861
pabloferreri <pab.ferreri@gmail.com>
2021-01-14 17:00:52 -0300

    login and register

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ app.listen(process.env.PORT || 3000, () =>{
 app.get("/", (req,res) =>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
 })
+
+app.get("/register", (req,res) =>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+})
+
+app.get("/login", (req,res) =>{
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+})
