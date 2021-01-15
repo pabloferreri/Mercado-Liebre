2ec064a79eef1d7965d2d768390f4a3500a3eb1f
pabloferreri <pab.ferreri@gmail.com>
2021-01-15 00:57:46 -0300

    register login pages

--- index.js
+++ index.js
@@ -15,10 +15,22 @@ app.get("/", (req,res) =>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
 })
 
+app.post("/", (req,res) =>{
+    res.sendFile(path.join(__dirname,"/views/index.html"));
+})
+
 app.get("/register", (req,res) =>{
     res.sendFile(path.join(__dirname,"/views/register.html"));
 })
 
+app.post("/register", (req,res) =>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+})
+
 app.get("/login", (req,res) =>{
+    res.sendFile(path.join(__dirname,"/views/login.html"));
+})
+
+app.post("/login", (req,res) =>{
     res.sendFile(path.join(__dirname,"/views/login.html"));
 })
